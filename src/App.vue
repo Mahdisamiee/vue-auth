89c3b4fff52c004b184e6bc69775430e4cc6e6a4
Mahdisamiee <34692316+Mahdisamiee@users.noreply.github.com>
2019-05-05 12:58:30 -0400

    base-login

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,38 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/about">About</router-link><span v-if="isLoggedIn"> | <a @click="logout">Logout</a></span>
     </div>
     <router-view/>
   </div>
 </template>
-
+<script>
+  export default {
+    computed : {
+      isLoggedIn : function(){ 
+        return this.$store.getters.isLoggedIn
+      }
+    },
+    methods: {
+      logout: function () {
+        this.$store.dispatch('logout')
+        .then(() => {
+          this.$router.push('/login')
+        })
+      }
+    },
+    created: function () {
+      this.$http.interceptors.response.use(undefined, function (err) {//this is a axios request and i just know it is for token :)
+        return new Promise(function (resolve, reject) {
+          if (err.status === 401 && err.config && !err.config.__isRetryRequest) {
+            this.$store.dispatch(logout)
+          }
+          throw err;
+        });
+    });
+  }
+  }
+</script>
 <style>
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
@@ -29,3 +55,4 @@
   color: #42b983;
 }
 </style>
+
--- src/main.js
+++ src/main.js
@@ -2,7 +2,18 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import Axios from 'axios'
 
+
+//for Axios and token
+
+Vue.prototype.$http = Axios;
+const token = localStorage.getItem('token')
+if (token) {
+  Vue.prototype.$http.defaults.headers.common['Authorization'] = token
+}
+
+//
 Vue.config.productionTip = false
 
 new Vue({
