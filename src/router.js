e7dc0fa0ffb1b19e16c69f6b5014360d0b63a63a
Mahdisamiee <34692316+Mahdisamiee@users.noreply.github.com>
2019-05-06 14:06:37 -0400

    auth-login

--- src/router.js
+++ src/router.js
@@ -2,21 +2,26 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import store from './store.js'
 
-import Home from './views/Home.vue'
-import About from './views/About.vue'
+import Home from './components/Home.vue'
+import About from './components/About.vue'
 import Login from './components/Login.vue'
 import Secure from './components/Secure.vue'
 import Register from './components/Register.vue'
 
 Vue.use(Router)
 
-let router = new Router({
+const router = new Router({
   mode: 'history',
   routes: [
     {
       path:'/',
       name: 'home',
       component : Home
+    },
+    {
+      path: '/about',
+      name: 'about',
+      component: About
     },
     {
       path: '/login',
@@ -36,11 +41,7 @@ let router = new Router({
         requiresAuth: true
       }
     },
-    {
-      path: '/about',
-      name: 'about',
-      component: About
-    }
+    
   ]
 })
 
